51fc2d4ca221ec3f847132acc1be5e1deaeeb0d7
Jaqen <591875624@qq.com>
2018-05-24 14:36:20 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-
-socket.setdefaulttimeout(5)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('47.52.171.108',22))
-a = sock.recv(512)
-print a
-sock.close()
